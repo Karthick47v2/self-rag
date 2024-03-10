7353fdd4426850ae273cf3f5709543d61e669464
karthick47v2 <karthicksharma11@gmail.com>
2024-03-10 11:28:15 +0800

    test

---
+++ temp.py
@@ -0,0 +1,26 @@
+from vllm import LLM, SamplingParams
+
+model = LLM("selfrag/selfrag_llama2_7b", dtype="half")
+sampling_params = SamplingParams(temperature=0.0, top_p=1.0, max_tokens=100, skip_special_tokens=False)
+
+def format_prompt(input, paragraph=None):
+  prompt = "### Instruction:\n{0}\n\n### Response:\n".format(input)
+  if paragraph is not None:
+    prompt += "[Retrieval]<paragraph>{0}</paragraph>".format(paragraph)
+  return prompt
+
+query_1 = "Leave odd one out: twitter, instagram, whatsapp."
+query_2 = "Can you tell me the difference between llamas and alpacas?"
+queries = [query_1, query_2]
+
+# for a query that doesn't require retrieval
+preds = model.generate([format_prompt(query) for query in queries], sampling_params)
+for pred in preds:
+  print("Model prediction: {0}".format(pred.outputs[0].text))
+
+
+# for a query that needs factual grounding
+prompt = format_prompt("Can you tell me the difference between llamas and alpacas?", "The alpaca (Lama pacos) is a species of South American camelid mammal. It is similar to, and often confused with, the llama. Alpacas are considerably smaller than llamas, and unlike llamas, they were not bred to be working animals, but were bred specifically for their fiber.")
+preds = model.generate([prompt], sampling_params)
+print([pred.outputs[0].text for pred in preds])
+# ['[Relevant]Alpacas are considerably smaller than llamas, and unlike llamas, they were not bred to be working animals, but were bred specifically for their fiber.[Fully supported][Utility:5]</s>']
